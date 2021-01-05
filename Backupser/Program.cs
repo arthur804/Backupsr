3f43f57eafc7d32d253278e03015a7b8d6651954
humble altmer <arthur804@gmail.com>
2021-01-05 02:24:42 +0100

    Update Program.cs

--- Backupser/Program.cs
+++ Backupser/Program.cs
@@ -4,40 +4,24 @@ using System.Windows.Forms;
 
 namespace Backupser {
 	public static class Program {
-		/// <summary>
-		/// The main entry point for the application.
-		/// </summary>
-		public const string STANDARD_LOCATION = "\\Backups-upsr";
+		static void Main(string[] args)
+        	{
+			if (args.Length > 2)
+				MakeBackup(args[0], args[1], args[2]);
+        	}
 		[STAThread]
-		static void Main(string[] s) {
-			if (s.Length != 0) {
-				try {
-					MakeBackup();
-				} catch (Exception e) {
-					MessageBox.Show(e.Message);
-				}
-			} else {
-				Application.EnableVisualStyles();
-				Application.SetCompatibleTextRenderingDefault(false);
-				Application.Run(new MainForm());
-			}
-		}
-
-		private static void MakeBackup() {
-			var workDir = Directory.GetCurrentDirectory() + STANDARD_LOCATION;
-			if (!Directory.Exists(workDir)) {
-				MessageBox.Show("Backups-upsr missing");
-				return;
-			}
-			var storage = new Storage(workDir + "\\options");
-			if (Directory.Exists(storage.FileName) && Directory.Exists(storage.Folder)) {
-				ZippingClass.ZipToFolder(storage.FileName, storage.Folder, DateTime.Now.ToString("yyyyMMddTHHmmssfff"));
-				int.TryParse(storage.MaxStrorage, out int val);
+		private static void MakeBackup(string target, string destination, string amoung)
+		{
+			var workDir = Directory.GetCurrentDirectory();
+			if (Directory.Exists(target) && Directory.Exists(destination))
+			{
+				ZippingClass.ZipToFolder(target, destination, DateTime.Now.ToString("yyyyMMddTHHmmss"));
+				int.TryParse(amoung, out int val);
 				if (val < 1) val = 100;
-					ZippingClass.DeleteUntillAmount(storage.Folder, val);
+				ZippingClass.DeleteUntillAmount(destination, val);
 			}
 			else
-				MessageBox.Show("Chosen directory missing");
+				Console.WriteLine("A directory missing");
 		}
 	}
 }
